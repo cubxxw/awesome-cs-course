d7c2c2e452286698d96bb0dc15ed765af1ecd410
xiongxinwei <3293172751nss@gmail.com>
2022-09-21 16:22:20 +0800

    xiongxinwei update

---
+++ java/code/module3/src/main/java/top/nsddd/module3/HelloController.java
@@ -0,0 +1,20 @@
+package top.nsddd.module3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")  //路径 Localhost:8080/hello
+    @ResponseBody
+    String name() {
+        return "hello world <h1>Hello World</h1>";
+    }
+
+    @RequestMapping("/my")
+    @ResponseBody
+    String my() {
+        return "<b>我叫xiongxinwei</b>很高兴认识你!!!!!!!!<del>谢谢</del>";
+    }
+}
--- java/code/module3/src/main/java/top/nsddd/module3/Module3Application.java
+++ java/code/module3/src/main/java/top/nsddd/module3/Module3Application.java
@@ -11,3 +11,4 @@ public class Module3Application {
 	}
 
 }
+
