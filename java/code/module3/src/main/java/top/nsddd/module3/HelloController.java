ced44e80c40f8c545cd8dfce1278e1339bada192
xiongxinwei <3293172751nss@gmail.com>
2022-09-29 11:09:18 +0800

    java -- web

--- java/code/module3/src/main/java/top/nsddd/module3/HelloController.java
+++ java/code/module3/src/main/java/top/nsddd/module3/HelloController.java
@@ -1,13 +1,17 @@
 package top.nsddd.module3;
 
+import ch.qos.logback.classic.spi.EventArgUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Scanner;
 
 @Controller
 public class HelloController {
     @RequestMapping("/hello")  //路径 Localhost:8080/hello
     @ResponseBody
+
     String name() {
         return "hello world <h1>Hello World</h1>";
     }
