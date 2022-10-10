835e30f9acef0b2be142e13871b40822902a82ca
xiongxinwei <3293172751nss@gmail.com>
2022-10-10 18:38:54 +0800

    xiongxinwei update

---
+++ java/code/Mybatis/src/main/java/org/example/mapper/UserMapper.kt
@@ -0,0 +1,15 @@
+package org.example.mapper
+
+public interface UserMapper {
+    /*
+    *
+    * mybatis面对接口编程的两个一致性。
+    * 1. mapper接口的映射文件namespca要和mapper接口的全限定名一致
+    * 2. mapper接口的方法名和映射文件中的statement的id一致
+    * 3. mapper接口的方法的输入参数类型和映射文件中statement的parameterType的类型一致
+    * 4. mapper接口的方法的返回值类型和映射文件中statement的resultType的类型一致
+    * */
+
+    /*调用sql语句*/
+
+}
